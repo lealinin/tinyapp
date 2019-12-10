daad427498be1d3e5e40070bc10edecaed2c7897
Lea Linin <lea.linin@gmail.com>
2019-12-10 23:44:28 +0000

    Add route for urls_new

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  let templateVars = {};
+  res.render("urls_new", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
